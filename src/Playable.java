31e8cbca379deaa2212945a7e6df69ea920249f7
wangzihao <wangzihao@localhost>
2025-10-06 17:24:36 -0700

    add Playable interface full code

--- src/Playable.java
+++ src/Playable.java
@@ -1,4 +1,21 @@
-
+/**
+ * Interface for objects that can be played as cards in the game.
+ */
 public interface Playable {
-
+/**
+ * 
+ */
+  void execute(Game game, Player player);
+  
+  /**
+   * get the name of the card
+   * @return card's name
+   */
+  String getName();
+  
+  /**
+   * get description of card
+   * @return card's description
+   */
+  String getDescription();
 }
